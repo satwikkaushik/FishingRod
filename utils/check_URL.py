76a6e96d4ad6fdeeaa2fc2edec9c276074254b31
satwikkaushik <satwikkaushik05@gmail.com>
2025-01-04 09:24:11 +0000

    Add a util to check a url

---
+++ utils/check_URL.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import base64
+
+# load the API_KEY file
+API_KEY = ""
+THRESHOLD = 0.0
+
+with open("config.json", "r") as file:
+    config = json.load(file)
+    API_KEY = config.get("api_key")
+    THRESHOLD = config.get("threshold")
+
+
+REQ_HEADER = {
+    "accept": "application/json",
+        "x-apikey": API_KEY
+}
+
+def check(url):
+    url = encode_url(url)
+
+    try:
+        VIRUSTOTAL_URL = f"https://www.virustotal.com/api/v3/urls/{url}"
+        response = requests.get(VIRUSTOTAL_URL, headers = REQ_HEADER)
+
+        # check for response status, auto generates exception if not 200
+        response.raise_for_status()
+
+        # parsing response
+        parsed_response = response.json()["data"]["attributes"]["last_analysis_stats"]
+        
+        analysis_details = {
+            "malicious_count" : parsed_response["malicious"],
+            "suspicious_count" : parsed_response["suspicious"],
+            "undetected_count" : parsed_response["undetected"],
+            "harmless_count" : parsed_response["harmless"],
+            "timeout_count" : parsed_response["timeout"]
+        }
+
+        return determine_result(analysis_details)
+    
+    except Exception as e:
+        print(e)
+        return -1
+
+def encode_url(url):
+    # base64 url encoding is required by virusTotal API
+    return base64.urlsafe_b64encode(url.encode()).decode().strip('=')
+
+def determine_result(analysis_details):
+    total_count = 0
+
+    for key, values in analysis_details.items():
+        total_count += values
+
+    if(analysis_details["malicious_count"]/total_count > THRESHOLD):
+        return 1
+
+    return 0
