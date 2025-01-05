94045430dd0c1eb85f48f67900a321a7124ef024
satwikkaushik <satwikkaushik05@gmail.com>
2025-01-05 16:29:29 +0000

    Add feature to extract web content in util/extractor

--- utils/extractor.py
+++ utils/extractor.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-def extract_URL(url):
-    # checking format of URL
+def format_URL(url):
     if("https://" not in url):
         url = "https://" + url
+
+    return url
+
+def extract_URL(url):
+    url = format_URL(url)
 
     try:
         # fetching HTML content of the page
@@ -21,3 +26,29 @@ def extract_URL(url):
     except Exception as e:
         print(f"Error fetching the URL: {e}")
         return []
+
+def extract_content(url):
+    url = format_URL(url)
+
+    try:
+        # fetching HTML content of the page
+        response = requests.get(url)
+
+        # check for response status, auto generates exception if not 200
+        response.raise_for_status()
+
+        # parsing HTML contents
+        parsed_text = BeautifulSoup(response.text, 'html.parser')
+
+        for a_tag in parsed_text.find_all('a'):
+            a_tag.unwrap()  # removing <a> tag but keeping the text inside
+
+        # extracting text
+        text_content = parsed_text.get_text(separator='\n').strip()
+        text_content = re.sub(r'\n+', '\n', text_content).strip()
+
+        return text_content
+
+    except Exception as e:
+        print(f"Error fetching the URL: {e}")
+        return ""
